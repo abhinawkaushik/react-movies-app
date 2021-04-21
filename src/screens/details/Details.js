06578d68efb1cd1fe943207a75853c363ce43582
Abhinaw Kaushik <abhinaw@live.in>
2021-04-21 17:57:35 +0530

    dded ‘Back to Home’ button and YouTube trailer on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react'
+import ReactDOM from 'react-dom';
 import movieData from '../../common/movieData';
 import Header from '../../common/header/Header';
 import Typography from '@material-ui/core/Typography';
 import '../details/Details.css';
+import Home from '../home/Home';
+import Youtube from 'react-youtube';
 class Details extends Component{
     constructor(){
         super();
@@ -18,18 +21,33 @@ class Details extends Component{
 
         this.setState({currentState})        
     }
+    backtohomehandler=()=>{
+        ReactDOM.render(<Home/>,document.getElementById('root'))
+    }
     render(){       
         let movie = this.state.movie; 
+        const opts ={
+            height:'300',
+            width:'700',
+            playerVars:{
+                autplay:1
+            }
+        }
         return(
             <div className="details">
                 <Header/>
+                <div className="back">
+                <Typography onClick={this.backtohomehandler}>
+                    &#60; Back To Home
+                </Typography>
+                </div>
                 <div className="flex-containerDetails">
                 <div className="leftDetails">
                     <img src={movie.poster_url} alt={movie.title}/>
                 </div>
                 <div className="middleDetails">
                     <div>
-                    <Typography variant="headline" component="h2">{movie.title}</Typography>
+                    <Typography variant="h2" component="h2">{movie.title}</Typography>
                     </div>
                     <br/><br/>
                     <div>
@@ -56,6 +74,15 @@ class Details extends Component{
                         <Typography>
                             <span className="bold">Plot:</span><a href={movie.wiki_url} >(Wiki Link)</a> {movie.storyline}
                         </Typography>
+                    </div>
+                    <div className="trailerContainer">
+                        <Typography><span className="bold">Trailer:</span></Typography>
+                        <Youtube
+                        videoId={movie.trailer_url.split('?v=')[1]}
+                        opts={opts}
+                        onReady={this._onReady}
+                        />
+
                     </div>
                 </div>
                 <div className="rightDetails">
